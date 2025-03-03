22599564203fc0c93ee3973d44686cff661c58a1
ariba <naseramirza@Aribas-MacBook-Pro.local>
2025-03-03 15:24:52 +0400

    added docstrings to resolve errors

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,20 @@
-def sum(a, b):
+"""
+docstrings
+"""
+
+def add_numbers(a, b):
+    """
+    will returnn the sum of two numbers
+    """
     return a + b
 
 
-def hello(name):
+def hello(name: str):
+    """
+    will print a greeting message with the given name
+    """
     print(f"Hello, {name}!")
 
 
-hello("Alice")
-hello()
+
+hello("Alice")  #function calls
